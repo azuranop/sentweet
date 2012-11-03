a6f1dd1a8e8d77843e3562e6b5eb2007c6f72b64
azuranop <azuranop@yahoo.co.uk>
2012-11-03 14:31:52 +0000

    Changes

--- twapp/management/commands/tweext.py
+++ twapp/management/commands/tweext.py
@@ -1,98 +1,74 @@
 # -*- coding: utf-8 -*-
 
-import sys
-import time
 import twitter
-import MySQLdb
+import config 
 import pickle
 
-from extractwords import extractwords, word_feats
-from training import classifier, extract_features
 from django.core.management.base import BaseCommand
-from web.twapp.models import Tweet
-from database import login_db
+from webgit.twapp.models import Tweet
+from django.db.models import Max
+
+from extractwords import extractwords, word_feats
+
 from twitter_login import login
 from twitter_util import makeTwitterRequest
-from mysql_date import mysql_date
 
 class Command(BaseCommand):
+
     help = 'Retrieve the tweets and classify them with the Bayesian classifier'
 
     def handle(self, *args, **options):
+ 
+        # Get the configuration parameters
+
+        c = config.Config()
+        d = c.cfg
 
         # Load the classifier
 
         f = open('bayesclass.pickle')
         classifier = pickle.load(f)
-
-#         users = ['alonso', 'ladygaga', 'timoreilly']
-        user = 'ladygaga'
-
-        # For the Twitter API call
-
-        KW = { 'count': 200,
-                'skip_users': 'true',
-                'include_entities': 'true',
-                'since_id': 1,
-                'id': user }
-
+    
         t = login()
 
-        try:
 
-           conn = MySQLdb.connect(**login_db)
-           cursor = conn.cursor()
+        KW = {'user' : d.get('api', 'user'), 
+              'count' : d.get('api', 'count'), 
+              'skip_users' : d.get('api', 'skip_users'), 
+              'include_entities' : 'true',
+              'since_id' : 1, 
+              'id' : 2} 
 
-           # Select id of latest tweet in database
-           # Database needs to be setup before running this
-  
-           cursor.execute("SELECT max(tweet_id) FROM twapp_tweet WHERE user=%s",(user))
-           latestId = cursor.fetchone()
-           latestId = latestId[0]
+        p = Tweet.objects.aggregate(Max('tweet_id'))
+#        print(p)
+        latestId = p['tweet_id__max']
+        if latestId == None:
+           latestId = 1
 
-           if latestId == None:
-              latestId = 1
+        KW['since_id'] = int(latestId)
+        print(KW['since_id'])
 
-           KW['since_id'] = int(latestId)
-           print(KW['since_id'])
+        api_call = getattr(t.statuses, 'user_timeline')
+        tweets = makeTwitterRequest(t, api_call, **KW)
 
-           print KW
+        print 'Fetched %i tweets' % len(tweets)
 
-           api_call = getattr(t.statuses, 'user_timeline')
-           tweets = makeTwitterRequest(t, api_call, **KW)
+        a = len(tweets)           
 
-           print 'Fetched %i tweets' % len(tweets)
+        for i in range(a):
 
-           a = len(tweets)           
+            txt = tweets[i]['text']
+            ref = tweets[i]['id']
+            src = tweets[i]['source']
+            outc = int(classifier.classify(word_feats(txt)))
+            created = mysql_date(tweets[i]['created_at'])
 
-           for i in range(a):
+            q = Tweet( datetime = created, 
+                       user = K['user'],
+                       content = txt, 
+                       source = src,
+                       tweet_id = ref,
+                       prop = outc )
 
-              txt = tweets[i]['text']
-
-              print classifier.classify(word_feats(txt))
-
-              outc = int(classifier.classify(word_feats(txt)))
-
-              ref = tweets[i]['id']
-              src = tweets[i]['source']
-              created = mysql_date(tweets[i]['created_at'])
-
-              try:
-
-              #Insert into database 
-
-                cursor.execute ("INSERT INTO  twapp_tweet(user,content,source,tweet_id,datetime,prop) VALUES (%s,%s,%s,%s,%s,%s) ", (user,txt,src,ref,created,outc))
-              except MySQLdb.Error, e:
-                  print "Error %d: %s" % (e.args[0], e.args[1])
-
-           cursor.close()
-           conn.commit()
-           conn.close()
-           sys.exit(0)
-
-        except MySQLdb.Error, e:
-           print "Error %d: %s" % (e.args[0], e.args[1])
-           print "While handling %s" % (user)
-           sys.exit (1)
-
+            q.save()
         f.close()
